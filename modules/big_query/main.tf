e3a5fb07c5ac75b5108b9eb583ec58a2e68353ff
Valentin <wdcvalentin@yahoo.fr>
2023-05-04 16:50:25 +0200

    big query

---
+++ modules/big_query/main.tf
@@ -0,0 +1,29 @@
+resource "google_bigquery_dataset" "default" {
+  dataset_id                  = "foo"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+}
+
+resource "google_bigquery_table" "sheet" {
+  dataset_id = google_bigquery_dataset.default.dataset_id
+  table_id   = "sheet"
+
+  external_data_configuration {
+    autodetect    = true
+    source_format = "GOOGLE_SHEETS"
+
+    google_sheets_options {
+      skip_leading_rows = 1
+    }
+
+    source_uris = [
+      "https://docs.google.com/spreadsheets/d/1IjCImtz4bPQbOoj5_QpQw6kVCPL71a7ncrY9YMS2haA/edit#gid=437641014",
+    ]
+  }
+}
